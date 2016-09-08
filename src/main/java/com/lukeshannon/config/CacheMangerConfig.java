0a6a7c6839473ef3ab346da13bf5a1c7bde1b0e7
JohnPoulakos <John.Poulakos@scotiabank.com>
2016-09-08 17:10:40 -0400

    change redis endpoint

--- src/main/java/com/lukeshannon/config/CacheMangerConfig.java
+++ src/main/java/com/lukeshannon/config/CacheMangerConfig.java
@@ -27,7 +27,7 @@ public class CacheMangerConfig extends CachingConfigurerSupport {
 	  public RedisConnectionFactory redisConnectionFactory() {
 		CloudFactory cloudFactory = new CloudFactory();
         Cloud cloud = cloudFactory.getCloud();
-        RedisServiceInfo serviceInfo = new RedisServiceInfo("test", "http://127.0.0.1:6379");
+        RedisServiceInfo serviceInfo = new RedisServiceInfo("test", "http://172.30.217.165:6379");
         String serviceID = serviceInfo.getId();
         return cloud.getServiceConnector(serviceID, RedisConnectionFactory.class, null);
 	  }
