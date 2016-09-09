780c3ff1cc0d2be0d265a9e8eea7b642f32656b2
piggyvenus <piggyvenus@gmail.com>
2016-09-09 00:35:59 -0400

    Update CacheMangerConfig.java

--- src/main/java/com/lukeshannon/config/CacheMangerConfig.java
+++ src/main/java/com/lukeshannon/config/CacheMangerConfig.java
@@ -27,7 +27,7 @@ public class CacheMangerConfig extends CachingConfigurerSupport {
 	  public RedisConnectionFactory redisConnectionFactory() {
 		CloudFactory cloudFactory = new CloudFactory();
         Cloud cloud = cloudFactory.getCloud();
-        RedisServiceInfo serviceInfo = new RedisServiceInfo("test", "172.30.53.219:6379");
+        RedisServiceInfo serviceInfo = new RedisServiceInfo("test", "redis:6379");
         String serviceID = serviceInfo.getId();
         return cloud.getServiceConnector(serviceID, RedisConnectionFactory.class, null);
 	  }
